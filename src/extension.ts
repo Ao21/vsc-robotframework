71ecf53fb4601ed5d1b0bdc6a3ccf59bd9d831dd
Ronan Brett <Ronan.Brett@theaa.ie>
2016-04-01 16:56:22 +0100

    Remove Textmate Snippet Files

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,11 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import {Connect} from './server/connect';
+import {RFService} from './server/connect';
 import {RFCompletionItemProvider} from './providers/completion_provider';
 
 const ROBOTFRAMEWORK: vscode.DocumentFilter = { language: 'robot', scheme: 'file' }
 export function activate(context: vscode.ExtensionContext) {
-    console.log('activated!');
     context.subscriptions.push(vscode.languages.registerCompletionItemProvider('robot', new RFCompletionItemProvider(context)));
 }
 
